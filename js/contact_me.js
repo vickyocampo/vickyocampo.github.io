18bc2e93b2b2a05d6510b006248b4c84c628e71d
Mariano <marianof23@gmail.com>
2018-01-14 22:34:31 -0300

    Message success

--- js/contact_me.js
+++ js/contact_me.js
@@ -49,7 +49,7 @@ $(function() {
                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                     .append("</button>");
                 $('#success > .alert-success')
-                    .append("<strong>Your message has been sent. </strong>");
+                    .append("<strong>Ya sos parte de la #comunidadChuikChuik. </strong>");
                 $('#success > .alert-success')
                     .append('</div>');
 
